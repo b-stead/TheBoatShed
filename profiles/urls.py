9e9c9c8f78fe991802cfc2daf63af9662b57f151
Brendon stead <brendon_stead@yahoo.com>
2022-11-26 18:07:19 +0000

    added user update page

--- profiles/urls.py
+++ profiles/urls.py
@@ -8,5 +8,5 @@ app_name = "profiles"
 urlpatterns = [
     path("password_reset/", views.password_reset_request, name="password_reset"),
     path("<int:pk>/update/",views.ProfileUpdateView.as_view(success_url = reverse_lazy('home:home')), name="update"),
-    path("profiles_test/", views.profiles_test, name="profiles_test"),
+    #path("profiles_test/", views.profiles_test, name="profiles_test"),
 ]
